f76769484c662ff6c403183d7168bad59bfbcc8b
Sayuj Othayoth <sayuj@hatio.in>
2020-02-04 00:12:34 +0530

    Remove unwanted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :seasons
-  resources :contents
-  resources :purchases
-  resources :users do
-    resource :library
+  resources :movies, only: [:index]
+  resources :seasons, only: [:index]
+  resources :contents, only: [:index]
+  resources :purchases, only: [:create]
+  resources :users, only: [] do
+    resource :library, only: [:show]
   end
 end
